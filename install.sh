6b7c1f6d430632a6ac88d0e996bdfebe28d0a8d3
Hicham AIT TALGHALIT <hicham.aittalghalit@gmail.Com>
2022-02-19 16:19:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
 
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install xfce4 xfce4-goodies -y
+sudo apt install tightvncserver
+vncserver
+expect "Password:"
+send "$1\r"
+expect "Verify:"
+send "$1\r"
+expect "%"
+send "n"
+vncserver -kill :1
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+cp xstartup.txt ~/.vnc/xstartup
+sudo chmod +x ~/.vnc/xstartup
+sudo apt-get update -y
+sudo apt-get install firefox -y
+vncserver
