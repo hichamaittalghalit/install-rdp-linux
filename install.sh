cd988856ad195b43126ffad99c380ee40826704a
Hicham AIT TALGHALIT <hicham.aittalghalit@gmail.Com>
2022-02-19 16:51:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,6 @@ apt  install expect -y
 sudo apt install xfce4 xfce4-goodies
 sudo apt install tightvncserver -y
 vncserver
-expect "Password:"
-send "$1\r"
-expect "Verify:"
-send "$1\r"
-expect "%"
-send "n"
 vncserver -kill :1
 mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
 cp xstartup.txt ~/.vnc/xstartup
